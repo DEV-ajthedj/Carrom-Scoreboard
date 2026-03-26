c6df1abbdd35d77c018f476e40c087ad4ca5a094
DEV-ajthedj <theajayisbetter@gmail.com>
2026-03-26 19:54:10 -0400

    Added firebase config

---
+++ app/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqFT43zEqJQ_uqat5MYkxOQNVT0LRdpG4",
+  authDomain: "carrom-scoreboard.firebaseapp.com",
+  projectId: "carrom-scoreboard",
+  storageBucket: "carrom-scoreboard.firebasestorage.app",
+  messagingSenderId: "862163575261",
+  appId: "1:862163575261:web:e0ce2b920af7d48e661634"
+};
+
+const app = initializeApp(firebaseConfig);
